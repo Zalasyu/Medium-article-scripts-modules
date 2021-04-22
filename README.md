13b5c49e8dc46061909f477ffd9c8e71661b40cf
Zalasyu <alec_moldovan@yahoo.com>
2021-04-22 14:33:03 -0700

    Created a primitive calculator. Called the multiply function without the __name__ == '__main__'. Imported calc.py into main.py and ran main.py

---
+++ README.md
@@ -0,0 +1,1 @@
+ This repo contains files to explain the function of __name__ == '__main__' when importing modules.
---
+++ calc.py
@@ -0,0 +1,12 @@
+# A primitive calcuator simulator
+
+def multiply(num_1, num_2):
+    """Returns the product of two numbers"""
+    return num_1 * num_2
+
+def add(num_1, num_2):
+    """Returns the sum of two numbers"""
+    return num_1 + num_2
+
+result = multiply(3, 5)
+print(f'Here is your result {result}')
